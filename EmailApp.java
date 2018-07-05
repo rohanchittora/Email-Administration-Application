74d8013a23bd186dd0f4365ad65c80e6a317f442
rohanchittora <34954256+rohanchittora@users.noreply.github.com>
2018-07-05 08:50:30 +0530

    main

---
+++ EmailApp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emailapp;
+
+/**
+ *
+ * @author JWALA
+ */
+public class EmailApp {
+    
+    public static void main(String[] args) {
+        Email em1= new Email("Rohan", "Chittora");
+        System.out.println(em1.showInfo());;
+    }
+    
+}
